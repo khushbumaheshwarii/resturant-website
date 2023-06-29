2c3d02b7327d1bbcb6b81e39516526066a2fb09e
khushbumaheshwarii <khushbumaheshwari007@gmail.com>
2023-06-29 22:52:18 +0530

    styling the card part-1

--- src/component/basics/MenuCard.js
+++ src/component/basics/MenuCard.js
@@ -7,23 +7,25 @@ const MenuCard = ({ menuData }) => {
     {menuData.map((curElem) => {
         return(
         <>
-        <section className="main-card--cointainer" key={curElem.id}>
-            <div className="card-container">
-            <div className="card">
-                <div className="card-body">
-                    <span className="card-number card-circle subtle"> {curElem.id} </span>
-                    <span className="card-auther subtle"> {curElem.category} </span>
-                    <h2 className="card-tiltle"> {curElem.name} </h2>
-                    <span className="card-description subtle">
-                    {curElem.description}
-                    </span>
-                    <div className="card-read">Read</div>
+        
+            <section className="main-card--cointainer" key={curElem.id}>
+                <div className="card-container">
+                <div className="card">
+                    <div className="card-body">
+                        <span className="card-number card-circle subtle"> {curElem.id} </span>
+                        <span className="card-auther subtle"> {curElem.category} </span>
+                        <h2 className="card-tiltle"> {curElem.name} </h2>
+                        <span className="card-description subtle">
+                        {curElem.description}
+                        </span>
+                        <div className="card-read">Read</div>
+                    </div>
+                    <img src= {curElem.image} alt="" className="card-media" />
+                    <span className="card-tag">Order Now..</span>
                 </div>
-                <img src= {curElem.image} alt="" className="card-media" />
-                <span className="card-tag">Order Now..</span>
-            </div>
-            </div>
-        </section>
+                </div>
+            </section>
+        
         </>
         )
     })}
